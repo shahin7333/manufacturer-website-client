cf368d6da7ea256bb749bf3885838436d96fa2e2
shaparan shahin <shaparan7333@gmail.com>
2022-05-26 19:24:24 +0600

    upade about us

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,7 +3,9 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>this is about</h1>
+            <h1 className='text-3xl font-bold text-center text-secondary my-16'>About Us</h1>
+        <p className='text-center mx-12'>Our company provide you the best tools for your shop. Our all electrical tools are number one company's products. All of tools has werrenty and garanty. And we hope that all of tools are good. You never be dissapointed if order to us.</p>
+        <p className='text-center font-bold'>Best wishes</p>
         </div>
     );
 };
